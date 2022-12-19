02b80499a89d7cbc46b3ecf9c960546dfc3b7e71
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2022-12-19 19:58:47 +0300

    add Note table test server

--- server/test.js
+++ server/test.js
@@ -1,19 +1,25 @@
-const request = require('supertest');
-const Sequelize = require('sequelize');
-const server = require('./index.js');
+const request = require("supertest");
+const Sequelize = require("sequelize");
+const { DataTypes } = require("sequelize");
+const server = require("./index.js");
 
 beforeEach(async () => {
-  let mockedSequelize = new Sequelize('calendar', 'postgres', 'postgres', {
-    host: 'localhost',
-    dialect: 'postgres',
+  let mockedSequelize = new Sequelize("calendar", "postgres", "postgres", {
+    host: "localhost",
+    dialect: "postgres",
+  });
+  const Note = mockedSequelize.define("calendar", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    date: { type: DataTypes.DATE },
+    text: { type: DataTypes.STRING },
   });
   await mockedSequelize.sync({ force: true });
 });
 
-test('/ping', async () => {
+test("/ping", async () => {
   await request(server)
-    .get('/ping')
-    .expect('Content-Type', /json/)
+    .get("/ping")
+    .expect("Content-Type", /json/)
     .expect(200)
     .expect(({ body }) => {
       expect(body).toEqual({ pong: true });
